ccb652463e90e453db849a9e8d3414550c9cf42e
まさたか <shinonome419@icloud.com>
2022-08-27 21:51:05 +0900

    update: ProblemSentence()をConsumerWidgetへ変更

--- lib/app.dart
+++ lib/app.dart
@@ -7,8 +7,12 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ProblemPage(),
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ProblemPage(),
     );
   }
 }
--- lib/components/problem_sentence.dart
+++ lib/components/problem_sentence.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ProblemSentence extends StatelessWidget {
+//問題文
+class ProblemSentence extends ConsumerWidget {
   const ProblemSentence({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const Center(
       child: Text("ProblemSentence"),
     );
