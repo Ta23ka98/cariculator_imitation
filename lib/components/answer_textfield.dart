5d770becba27e337d6e34035e936f804140c77e5
まさたか <shinonome419@icloud.com>
2022-08-27 23:06:57 +0900

    clean: AnswerTextFieldをConsumerStatefulWidgetへ変更

--- lib/components/answer_textfield.dart
+++ lib/components/answer_textfield.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AnswerTextField extends StatelessWidget {
+import 'problem_state.dart';
+
+//回答テキストフィールド
+class AnswerTextField extends ConsumerStatefulWidget {
   const AnswerTextField({Key? key}) : super(key: key);
 
   @override
+  ConsumerState<AnswerTextField> createState() => _AnswerTextFieldState();
+}
+
+class _AnswerTextFieldState extends ConsumerState<AnswerTextField> {
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("AnswerTextField"),
-    );
+    return Container();
   }
 }
