c078c8f0366685c09b49a1ba42df56a02f01419e
まさたか <shinonome419@icloud.com>
2022-08-27 21:46:28 +0900

    update: ProblemPage()を変更
    ・ProblemSentence(),AnswerTextField(),CheckButton()を追加。
    ・追加されたページをProblemPage()に追加。

---
+++ lib/components/problem_sentence.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ProblemSentence extends StatelessWidget {
+  const ProblemSentence({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("ProblemSentence"),
+    );
+  }
+}
--- lib/problem_page.dart
+++ lib/problem_page.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'components/problem_sentence.dart';
+import 'components/answer_textfield.dart';
+import 'components/check_button.dart';
 
 class ProblemPage extends StatelessWidget {
   const ProblemPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("ProblemPage")),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('算数問題'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: const [
+            SizedBox(height: 16),
+            ProblemSentence(),
+            SizedBox(height: 16),
+            AnswerTextField(),
+            SizedBox(height: 16),
+            SizedBox(
+              width: double.infinity,
+              child: CheckButton(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
