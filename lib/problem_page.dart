5e309f534acf472fb01235de61f4d77753773c72
まさたか <shinonome419@icloud.com>
2022-08-27 23:45:44 +0900

    clean: ProblemPageをConsumerWidgetへ変更

--- lib/problem_page.dart
+++ lib/problem_page.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'components/problem_sentence.dart';
 import 'components/answer_textfield.dart';
 import 'components/check_button.dart';
 
-class ProblemPage extends StatelessWidget {
+class ProblemPage extends ConsumerWidget {
   const ProblemPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('算数問題'),
